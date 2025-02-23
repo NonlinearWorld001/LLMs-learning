c625846a6f47c2d53270e0c977ad7aba123b1718
Pual ZHU <1799486899@qq.com>
2025-02-23 10:19:44 +0800

    250223 LMconfig 参数更新

--- model/LMConfig.py
+++ model/LMConfig.py
@@ -5,28 +5,28 @@ class LMConfig(PretrainedConfig):
     model_type = "miniDeepSeek"
 
     def __init__(
-            self,
-            dim: int=512,
-            n_layers: int=8,
-            n_heads: int=16,   # 总注意力头数（查询头数量），决定query向量的并行计算能力
-            n_kv_heads: int=8, # 键/值头的数量，决定key/value向量的复用程度
+            self,  
+            dim: int=512,                  # 每个token的特征维度
+            n_layers: int=8,               # transformer的层数 ？
+            n_heads: int=16,               # 总注意力头数（查询头数量），决定query向量的并行计算能力
+            n_kv_heads: int=8,             # 键/值头的数量，决定key/value向量的复用程度
             # 每个key/value头会被多个query头共享（本例中16/8=2个query头共享1个key/value头，这个比例和标准transformer相比降低了50%参数量）
-            vocab_size: int=6400,
-            hidden_dim: int=None,
-            multiple_of: int=64,
-            norm_eps: float=1e-5,
-            max_seq_length: int=512,
-            dropout: float=0.0,
-            flash_attention: bool=True,
-            use_moe: bool=True,
-            num_experts_per_token=3,
-            n_routed_experts=8,
-            n_shared_experts: bool = True,
-            scoring_func='softmax',
-            aux_loss_alpha=0.01,
-            seq_aux=True,
-            norm_topk_prob=True,
-            **kwargs,
+            vocab_size: int=6400,          # 词汇表大小，会影响token序列长度
+            hidden_dim: int=None,          # 隐藏层维度，决定了模型的表达能力
+            multiple_of: int=64,           # 隐藏层维度的倍数，决定了模型的计算效率
+            norm_eps: float=1e-5,          # 归一化层的稳定性参数(防止除以零的极小数)
+            max_seq_length: int=512,       # 最大序列长度，决定了模型可以处理的最大token数量
+            dropout: float=0.0,            # 随机失活层的概率，防止过拟合
+            flash_attention: bool=True,    # 是否使用flash attention，提高计算效率
+            use_moe: bool=True,            # 是否使用混合专家机制，提高模型表达能力
+            num_experts_per_token=3,       # 每个token选择的专家数量
+            n_routed_experts=8,            # 总的专家数量
+            n_shared_experts: bool = True, # 是否共享专家
+            scoring_func='softmax',        # 评分函数，默认为'softmax'
+            aux_loss_alpha=0.01,           # 辅助损失的alpha参数
+            seq_aux=True,                  # 是否在序列级别上计算辅助损失
+            norm_topk_prob=True,           # 是否标准化top-k概率
+            **kwargs,                      
     ):
         self.dim = dim
         self.dim = dim
@@ -41,11 +41,11 @@ class LMConfig(PretrainedConfig):
         self.dropout = dropout
         self.flash_attention = flash_attention
         self.use_moe = use_moe
-        self.num_experts_per_token = num_experts_per_token  # 每个token选择的专家数量
-        self.n_routed_experts = n_routed_experts  # 总的专家数量
-        self.n_shared_experts = n_shared_experts  # 共享专家
-        self.scoring_func = scoring_func  # 评分函数，默认为'softmax'
-        self.aux_loss_alpha = aux_loss_alpha  # 辅助损失的alpha参数
-        self.seq_aux = seq_aux  # 是否在序列级别上计算辅助损失
-        self.norm_topk_prob = norm_topk_prob  # 是否标准化top-k概率
+        self.num_experts_per_token = num_experts_per_token  
+        self.n_routed_experts = n_routed_experts  
+        self.n_shared_experts = n_shared_experts  
+        self.scoring_func = scoring_func  
+        self.aux_loss_alpha = aux_loss_alpha  
+        self.seq_aux = seq_aux  
+        self.norm_topk_prob = norm_topk_prob  
         super().__init__(**kwargs)
